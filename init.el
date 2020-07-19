966b748fbed0620eaa98f54d994a27890b676117
marshall <marshall@marshallwhite.org>
2020-07-19 01:31:00 -0400

    Bring emacs into the cmake-ide age

--- init.el
+++ init.el
@@ -211,6 +211,12 @@
    'c++-mode-hook
    (lambda () (setq flycheck-clang-language-standard "c++17"))))
 
+;; I want better syntax checking for my CMake projects
+(use-package cmake-ide
+  :ensure t
+  :config
+  (cmake-ide-setup))
+
 ;; Improves C++ code style
 (setq c-basic-offset 4
       c-default-style "stroustrup") ; style from his book
@@ -225,13 +231,19 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(backup-directory-alist (quote (("." . "~/.emacs.d/backups"))))
+ '(c-basic-offset 4)
  '(custom-safe-themes
    (quote
     ("1119fc59c71d953f66e1b24889886f91ead269831f3e0562cd64b1781cc125c8" "82358261c32ebedfee2ca0f87299f74008a2e5ba5c502bde7aaa15db20ee3731" "6bc387a588201caf31151205e4e468f382ecc0b888bac98b2b525006f7cb3307" default)))
  '(indent-tabs-mode nil)
  '(initial-buffer-choice "~/files/pictures/mwhite_pixel.png")
+ '(kill-whole-line t)
  '(line-spacing 0.2)
+ '(package-selected-packages
+   (quote
+    (cmake-ide helm-projectile projectile flycheck python-docstring mentor org-pomodoro conda ein cyberpunk-theme poet-dark-monochrome-theme poet-dark-theme winum org-bullets helm-swoop helm use-package)))
  '(safe-local-variable-values (quote ((TeX-engine . xetex))))
+ '(whitespace-global-modes nil)
  '(whitespace-line-column 80)
  '(whitespace-style
    (quote
